9500f8cafeb069db87dabd2c9eaf23253c2c3859
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2018-10-21 19:06:12 +0530

    Update setup-gce.sh

--- setup-gce.sh
+++ setup-gce.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 sudo apt-get -y update
 sudo apt-get -y upgrade
@@ -14,9 +15,9 @@ source activate fastai-v1
 conda install -y -c pytorch pytorch-nightly cuda92
 conda install -y -c fastai torchvision-nightly
 conda install -y -c fastai fastai
-python -m ipykernel install --user --name fastai-v1 --display-name "fastai-v1"
 
-jupyter notebook --generate-config
+source activate fastai-v1
+python -m ipykernel install --user --name fastai-v1 --display-name "fastai-v1"
 
 git clone https://github.com/fastai/course-v3.git
 
@@ -42,46 +43,5 @@ EOL
 sudo mv /tmp/jupyter.service /lib/systemd/system/jupyter.service
 sudo systemctl start jupyter.service
 sudo systemctl enable jupyter.service
-
-## Create the onboot script
-cat > onboot.sh <<EOL
-echo "started: $(date)" > $HOME/onboot-status
-#!/bin/bash
-# Update fastai
-source activate fastai-v1
-conda install -y -c pytorch pytorch-nightly cuda92
-conda install -y -c fastai torchvision-nightly
-conda install -y -c fastai fastai
-python -m ipykernel install --user --name fastai-v1 --display-name "fastai-v1"
-
-# Update course v3
-cd course-v3
-git checkout .
-git pull origin master
-
-# Add your code to run at the startup
-
-# Make sure we've updated the onboot
-echo "completed: $(date)" > $HOME/onboot-status
-EOL
-chmod +x onboot.sh
-
-## Run the above onboot script on start
-cat > /tmp/onboot.service <<EOL
-[Unit]
-Description=onboot
-After=network.target
-
-[Service]
-Type=oneshot
-User=$USER
-ExecStart=$HOME/onboot.sh
-
-[Install]
-WantedBy=multi-user.target
-EOL
-
-sudo mv /tmp/onboot.service /lib/systemd/system/onboot.service
-sudo systemctl enable onboot.service
 
 sudo reboot
