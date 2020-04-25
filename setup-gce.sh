46ca945ebc7d7b41986dc18746b4ae685e05095b
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2020-04-25 23:22:20 +0530

    Update gpu driver install code.

--- setup-gce.sh
+++ setup-gce.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 set -e
 
-sudo add-apt-repository -y ppa:graphics-drivers/ppa
-sudo apt install -y nvidia-driver-396
+curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+
+sudo apt-get update
+sudo apt-get install cuda
+
+nvidia-smi
 
 # This will use python command at the end and there's no such command.
 # So, we need to ignore that command.
