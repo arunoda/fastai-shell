c717751f4243399f985ce750c1c1ef7a544590fa
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2022-09-30 19:14:47 +0530

    Update for new commands

--- setup-gce.sh
+++ setup-gce.sh
@@ -3,27 +3,27 @@ set -e
 
 sudo add-apt-repository -y ppa:graphics-drivers/ppa
 sudo apt update
-sudo apt install -y nvidia-driver-440
+sudo apt install -y nvidia-driver-515
 
 # This will use python command at the end and there's no such command.
 # So, we need to ignore that command.
 set +e
-wget https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
-bash Anaconda3-2019.10-Linux-x86_64.sh -b -p $HOME/anaconda3
+wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
+bash Anaconda3-2022.05-Linux-x86_64.sh -b -p $HOME/anaconda3
 set -e
 
 # This will allow us to use conda.
 # source ~/.bashrc has no effect here: https://stackoverflow.com/a/43660876/457224
 export PATH="$HOME/anaconda3/bin:$PATH"
 
-conda create -y --name fastai-v1 python=3.7
+conda create -y --name fastai python=3.9
 
-source activate fastai-v1
+source activate fastai
 
 conda install -y -c pytorch -c fastai fastai
 conda install -y ipykernel
 
-python -m ipykernel install --user --name fastai-v1 --display-name "fastai-v1"
+python -m ipykernel install --user --name fastai --display-name "fastai"
 
 git clone https://github.com/fastai/course-v4.git
 
@@ -59,7 +59,7 @@ c.NotebookApp.token = ''
 c.NotebookApp.ip = '0.0.0.0'
 c.NotebookApp.port = 8080
 
-c.KernelSpecManager.whitelist = ["fastai-v1"]
+c.KernelSpecManager.whitelist = ["fastai"]
 EOL
 
 ## Add the update fastai script
@@ -67,17 +67,17 @@ cat > ~/update-fastai.sh <<EOL
 #!/bin/bash
 
 export PATH=$HOME/anaconda3/bin:$PATH
-source activate fastai-v1
+source activate fastai
 conda install -c pytorch
 pip install fastai2
 pip install nbdev
 conda install pyarrow
-pip install pydicom kornia opencv-python scikit-image graphviz azure azure-cognitiveservices-vision-computervision azure-cognitiveservices-search-websearch azure-cognitiveservices-search-imagesearch "ipywidgets>=7.5.1" sentencepiece scikit_learn
+pip install pydicom kornia opencv-python scikit-image graphviz "ipywidgets>=8.0.2" sentencepiece scikit_learn
 
 sudo systemctl restart jupyter
 EOL
 
 chmod +x ~/update-fastai.sh
 
-# allow users to install stuff to fastai-v1 conda env directly.
-echo "source activate fastai-v1" >> ~/.bashrc
+# allow users to install stuff to fastai conda env directly.
+echo "source activate fastai" >> ~/.bashrc
